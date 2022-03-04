f5080f7d0981de03c2b8ef74ebd59aca70c05ae2
Brandon Hoskins <branjames117@gmail.com>
2022-03-03 22:06:34 -0600

    added JWT authentication to GraphQL queries and mutations

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
 // import ApolloServer
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,6 +15,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware,
   });
 
   // start the Apollo server
@@ -32,7 +34,7 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once("open", () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running at http://localhost:${PORT}`);
   });
